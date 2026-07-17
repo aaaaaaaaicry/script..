0b9eb7c976d550521e3fb67322031be23d067378
CHIEF <brennoezequiel2026@gmail.com>
2026-07-17 14:12:39 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
 
+if pcall(function() 
+    if debug and debug.getinfo then 
+        error("debug") 
+    end 
+end) then
+    os.exit()
+end
+print((function() local t={(25+3),(58*1),(3*1),(0x4),(6+40)}; local r=""; for i=(1*1),#t do r=r..string.char(t[i]~(126*1)) end; return r end)())
